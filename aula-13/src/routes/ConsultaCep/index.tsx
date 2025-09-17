c94e54cc6165e2e0ccc1b937e0c3f326f0c5b07e
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2025-09-17 15:31:31 -0300

    issues: Corrigido o problema de chamada para a api de cep.

--- aula-13/src/routes/ConsultaCep/index.tsx
+++ aula-13/src/routes/ConsultaCep/index.tsx
@@ -1,53 +1,70 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import type { TipoCep } from "../../types/tipoCep";
+import { useNavigate } from "react-router-dom";
 
 export default function ConsultaCep() {
 
-  const [logradouro, setLogradouro] = useState<TipoCep>();
+    
 
-  const fetchData = async (cepParam:number) => {
+    const [logradouro, setLogradouro] = useState<TipoCep>();
 
-    const response = await fetch(`https://viacep.com.br/ws/${cepParam}/json/`);
+  const fetchData = async (
+    cepParam: string,
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    try {
+        e.preventDefault();
+        const response = await fetch(`https://viacep.com.br/ws/${cepParam}/json/`);
 
-    const data:TipoCep = await response.json();
+        const data: TipoCep = await response.json();
+        setLogradouro(data);
 
-    setLogradouro(data);
+    } catch (error) {
+        console.error(error);
 
+    }
   };
 
-  const [cepProcurado, setCepProcurado] = useState<number>(0);
+  const [cepProcurado, setCepProcurado] = useState<string>("");
 
   return (
     <main>
       <h1>Consulta CEP</h1>
 
-        <div>
-            <form onSubmit={()=>fetchData(cepProcurado)}>
-                <fieldset>
-                    <legend>Pesquis de CEP</legend>
-                    <div>
-                        <label htmlFor="idCep">CEP buscado:</label>
-                        <input type="number" name="cep" maxLength={8} required={true} placeholder="Digite o cep a pesquisar:" value={cepProcurado} onChange={(e)=> setCepProcurado(Number(e.target.value))}/>
-                    </div>
-                    <div>
-                        <button type="submit">Pesquisar</button>
-                    </div>
-                </fieldset>
-            </form>
-        </div>
-        <div>
-            {
-                logradouro ?
-                <div>
-                    <p>Bairro : {logradouro.bairro}</p>
-                    <p>Cep : {logradouro.cep}</p>
-                    <p>Rua : {logradouro.logradouro}</p>
-                    <p>Local : {logradouro.localidade}</p>
-                </div>
-                : <p>Localidade não encontrada.</p>
-
-            }
-        </div>
+      <div>
+        <form onSubmit={(e) => fetchData(cepProcurado, e)}>
+          <fieldset>
+            <legend>Pesquis de CEP</legend>
+            <div>
+              <label htmlFor="idCep">CEP buscado:</label>
+              <input
+                type="text"
+                name="cep"
+                maxLength={8}
+                required={true}
+                placeholder="Digite o cep a pesquisar:"
+                value={cepProcurado}
+                onChange={(e) => setCepProcurado(e.target.value)}
+              />
+            </div>
+            <div>
+              <button type="submit">Pesquisar</button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+      <div>
+        {logradouro ? (
+          <div>
+            <p>Bairro : {logradouro.bairro}</p>
+            <p>Cep : {logradouro.cep}</p>
+            <p>Rua : {logradouro.logradouro}</p>
+            <p>Local : {logradouro.localidade}</p>
+          </div>
+        ) : (
+          <p>Localidade não encontrada.</p>
+        )}
+      </div>
     </main>
   );
 }
