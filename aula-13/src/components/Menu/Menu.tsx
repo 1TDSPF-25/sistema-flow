835744a646913969ab62c8bf8a81997824a5204c
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2025-09-17 09:07:33 -0300

    Link para a rota de consultar cep criado.

--- aula-13/src/components/Menu/Menu.tsx
+++ aula-13/src/components/Menu/Menu.tsx
@@ -5,7 +5,8 @@ export default function Menu(){
     return(
         <nav>
             <Link to="/">Home </Link>| 
-            <Link to="/produtos"> Produtos</Link>
+            <Link to="/produtos"> Produtos</Link> |
+            <Link to="/consulta-cep">Consultar Cep</Link>
         </nav>
     );
 }
