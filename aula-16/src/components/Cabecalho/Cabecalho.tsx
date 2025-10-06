603085eb89d3c32e8950617d44479c865f03cf0e
HenriqueTcesar <rickcesar1717@gmail.com>
2025-10-06 19:22:17 -0300

    feat: Implementado barra de pesquisa por nome

--- aula-16/src/components/Cabecalho/Cabecalho.tsx
+++ aula-16/src/components/Cabecalho/Cabecalho.tsx
@@ -1,11 +1,19 @@
 import Menu from "../Menu/Menu";
+import { useState } from "react";
+export default function Cabecalho() {
+  const [termo, setTermo] = useState("");
 
-export default function Cabecalho(){
+  const handleSearch = () => {
+    localStorage.setItem("termoPesquisa", termo);
+    window.dispatchEvent(new Event("storage"));
+  };
 
-    return(
-        <header>
-            <h1>Cabeçalho</h1>
-            <Menu/>
-        </header>   
-    );
+  return (
+    <header>
+      <h1>Cabeçalho</h1>
+      <input type="text" placeholder="Pesquisar produtos eletrônicos" value={termo} onChange={(e) => setTermo(e.target.value)}/>
+      <button onClick={handleSearch}>Buscar</button>
+      <Menu />
+    </header>
+  );
 }
