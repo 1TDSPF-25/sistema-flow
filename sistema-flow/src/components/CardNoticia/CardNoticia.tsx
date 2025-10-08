1fb5deb0fab8f994a0c7d9b0ebc3b42208c8d47a
EnzoVazz <evaz0711@gmail.com>
2025-10-08 10:14:02 -0300

    feat: Iniciando a estilização do CardNoticia

--- sistema-flow/src/components/CardNoticia/CardNoticia.tsx
+++ sistema-flow/src/components/CardNoticia/CardNoticia.tsx
@@ -1,14 +1,14 @@
 export default function CardNoticia(){
     return(
-        
-        <div className="grid grid-cols-3 gap-4">
-        <div className="">Noticia1</div>
-        <div className="">Noticia2</div>
-        <div className="">Noticia3</div>
-        <div className="">Noticia4</div>
-        <div className="">Noticia5</div>
-        <div className="">Noticia6</div>
+        <div className="flex justify-center w-full"> 
+            <div className="grid grid-cols-3">
+                <div className="border-2 p-5 m-3 flex justify-center w-fit">Noticia1</div>
+                <div className="border-2 p-5 m-3 flex justify-center w-fit">Noticia2</div>
+                <div className="border-2 p-5 m-3 flex justify-center w-fit">Noticia3</div>
+                <div className="border-2 p-5 m-3 flex justify-center w-fit">Noticia4</div>
+                <div className="border-2 p-5 m-3 flex justify-center w-fit">Noticia5</div>
+                <div className="border-2 p-5 m-3 flex justify-center w-fit">Noticia6</div>
+            </div>
         </div>
-
     )
 }
