30063c17ead83c93a2e6a38bddac7079d5a6caac
PietroDonella <pietrosalomao1@gmail.com>
2025-10-08 09:34:55 -0300

    feat: Criando estrutura grid para as Noticías.

--- sistema-flow/src/components/CardNoticia/CardNoticia.tsx
+++ sistema-flow/src/components/CardNoticia/CardNoticia.tsx
@@ -1,8 +1,13 @@
 export default function CardNoticia(){
     return(
         
-        <div>
-            <h1>Noticia1</h1>
+        <div className="grid grid-cols-3 gap-4">
+        <div className="">Noticia1</div>
+        <div className="">Noticia2</div>
+        <div className="">Noticia3</div>
+        <div className="">Noticia4</div>
+        <div className="">Noticia5</div>
+        <div className="">Noticia6</div>
         </div>
 
     )
