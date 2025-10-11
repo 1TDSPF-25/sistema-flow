de223e77f8443b1df36046d4a551fa2ed4e0a7c9
EnzoVazz <evaz0711@gmail.com>
2025-10-11 17:42:44 -0300

    feat: Finalizando a estilização do CardNoticia

--- sistema-flow/src/components/CardNoticia/CardNoticia.tsx
+++ sistema-flow/src/components/CardNoticia/CardNoticia.tsx
@@ -1,13 +1,13 @@
 export default function CardNoticia(){
     return(
-        <div className="flex justify-center w-full"> 
-            <div className="grid grid-cols-3">
-                <div className="border-2 p-5 m-3 flex justify-center w-fit">Noticia1</div>
-                <div className="border-2 p-5 m-3 flex justify-center w-fit">Noticia2</div>
-                <div className="border-2 p-5 m-3 flex justify-center w-fit">Noticia3</div>
-                <div className="border-2 p-5 m-3 flex justify-center w-fit">Noticia4</div>
-                <div className="border-2 p-5 m-3 flex justify-center w-fit">Noticia5</div>
-                <div className="border-2 p-5 m-3 flex justify-center w-fit">Noticia6</div>
+        <div className="flex justify-center w-screen">
+            <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 text-wrap text-sm md:text-base lg:text-xl">
+                <div className="border-2 p-5 m-3 justify-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam totam eum nam molestiae eaque, consectetur rerum voluptate nobis autem voluptatem.</div>
+                <div className="border-2 p-5 m-3 justify-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam totam eum nam molestiae eaque, consectetur rerum voluptate nobis autem voluptatem.</div>
+                <div className="border-2 p-5 m-3 justify-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam totam eum nam molestiae eaque, consectetur rerum voluptate nobis autem voluptatem.</div>
+                <div className="border-2 p-5 m-3 justify-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam totam eum nam molestiae eaque, consectetur rerum voluptate nobis autem voluptatem.</div>
+                <div className="border-2 p-5 m-3 justify-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam totam eum nam molestiae eaque, consectetur rerum voluptate nobis autem voluptatem.</div>
+                <div className="border-2 p-5 m-3 justify-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam totam eum nam molestiae eaque, consectetur rerum voluptate nobis autem voluptatem.</div>
             </div>
         </div>
     )
