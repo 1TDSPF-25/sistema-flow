10635e95ba2e888b000e725ea27f2ccd20ee3ddc
FelipeFerrete <felipeferretelemes@gmail.com>
2025-10-13 09:47:44 -0300

    :sparkles: feat: Pagina Cadastro

--- sistema-flow/src/main.tsx
+++ sistema-flow/src/main.tsx
@@ -8,6 +8,7 @@ import Home from './routes/Home/index.tsx';
 import Produtos from './routes/Produtos/index.tsx';
 import EditarProdutos from './routes/EditarProdutos/index.tsx';
 import Error from './routes/Error/index.tsx';
+import CadastroFarmacia from './routes/Cadastro/index.tsx';
 
 export function rotaLimitada(elemento:React.ReactElement) {
   return localStorage.getItem('token') ? elemento : <Home/>
@@ -16,6 +17,7 @@ export function rotaLimitada(elemento:React.ReactElement) {
 const router = createBrowserRouter([
     {path:"/", element: <App/>, errorElement:<Error/>, children:[
     {path:"/", element: <Home/>},
+    {path:"/Cadastro", element:( <CadastroFarmacia/>)},
     {path:"/produtos", element:rotaLimitada( <Produtos/>)},
     {path:"/editar/produtos/:id", element: rotaLimitada(<EditarProdutos/>)},
   ]}
