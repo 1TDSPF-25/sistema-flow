a8498f08a256c4288d79acaeae0e2d91cca65a12
Buenozw <jvictorcastelini2006@gmail.com>
2025-10-13 21:18:17 -0300

    Iniciando inserção de imagens nos cards

--- sistema-flow/src/components/CardNoticia/CardNoticia.tsx
+++ sistema-flow/src/components/CardNoticia/CardNoticia.tsx
@@ -2,21 +2,64 @@ export default function CardNoticia(){
     return(
         <div className="flex justify-center w-screen bg-gray-100 py-10">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-5">
-
-                <div className="bg-white border rounded-lg shadow-lg p-6 hover:shadow-2xl transition-shadow duration-300">
-                    <h1 className="font-bold text-lg mb-3 text-gray-800">Corinthians Desafia Gigante Europeu e Conquista o Tricampeonato Mundial em Jogo Épico Contra o Real Madrid</h1>
-                    <p className="text-gray-600 text-sm md:text-base">Em uma final épica em Wembley, o Corinthians venceu o Real Madrid por 1 a 0 com um gol no final da partida e conquistou o tricampeonato mundial de clubes, com atuação heroica do jovem Gui Negão e mais uma "invasão" da Fiel Torcida em Londres.</p>
+            {/* Card Notícia 1 */}
+            <div className="bg-white border rounded-lg shadow-lg overflow-hidden hover:shadow-2xl transition-shadow duration-300">
+                <div className="overflow-hidden">
+                    <img
+                    src="https://lncimg.lance.com.br/cdn-cgi/image/width=950,quality=75,fit=pad,format=webp/uploads/2023/08/2-imagens-44.jpg"
+                    alt="Corinthians Campeão Mundial"
+                    className="w-full h-48 object-cover hover:scale-105 transition-transform duration-300"
+                    />
+                </div>
+                <div className="p-6">
+                    <h1 className="font-bold text-lg mb-3 text-gray-800">
+                    Corinthians Desafia Gigante Europeu e Conquista o Tricampeonato Mundial
+                    </h1>
+                    <p className="text-gray-600 text-sm md:text-base">
+                    Em uma final épica em Wembley, o Corinthians venceu o Real Madrid por 1 a 0 com um gol no final da partida e conquistou o tricampeonato mundial.
+                    </p>
+                </div>
                 </div>
 
-                <div className="bg-white border rounded-lg shadow-lg p-6 hover:shadow-2xl transition-shadow duration-300">
-                  <h1 className="font-bold text-lg mb-3 text-gray-800">Rebeca Andrade Conquista Ouro Inédito no Mundial e Eterniza Novo Movimento com seu Nome</h1>
-                  <p className="text-gray-600 text-sm md:text-base">Rebeca Andrade conquistou o ouro inédito no individual geral do Campeonato Mundial de Ginástica. Durante a competição, ela executou um novo salto que será oficialmente batizado como "Andrade II", consolidando seu status como a maior ginasta da história do Brasil.</p>
-                </div>
+            {/* Card Notícia 2 */}
 
-                <div className="bg-white border rounded-lg shadow-lg p-6 hover:shadow-2xl transition-shadow duration-300">
-                   <h1 className="font-bold text-lg mb-3 text-gray-800">Brasil Inicia Projeto-Piloto de "Currículo Flexível Invertido" em 50 Escolas de Ensino Médio</h1>
-                   <p className="text-gray-600 text-sm md:text-base">O MEC iniciou um projeto-piloto em 50 escolas brasileiras com um novo modelo de "currículo flexível". Nele, alunos estudam a teoria online e usam o tempo em sala de aula para desenvolver projetos práticos, visando maior engajamento e preparo para o mercado.</p>
-                </div>
+                <div className="bg-white border rounded-lg shadow-lg overflow-hidden hover:shadow-2xl transition-shadow duration-300">
+                    <div className="overflow-hidden">
+                        <img
+                        src="https://admin.cnnbrasil.com.br/wp-content/uploads/sites/12/2023/10/REBECA-e1698183867468.png?w=419&h=283&crop=0"
+                        alt="Rebeca Andrade Ouro Mundial"
+                        className="w-full h-48 object-cover hover:scale-105 transition-transform duration-300"
+                        />
+                    </div>
+                    <div className="p-6">
+                        <h1 className="font-bold text-lg mb-3 text-gray-800">
+                        Rebeca Andrade Conquista Ouro Inédito no Mundial
+                        </h1>
+                        <p className="text-gray-600 text-sm md:text-base">
+                        Rebeca Andrade conquistou o ouro inédito no individual geral e executou um novo salto que será oficialmente batizado como “Andrade II”.
+                        </p>
+                    </div>
+                    </div>
+
+                {/* Card Notícia 3 */}
+
+                <div className="bg-white border rounded-lg shadow-lg overflow-hidden hover:shadow-2xl transition-shadow duration-300">
+                    <div className="overflow-hidden">
+                        <img
+                        src="https://admin.cnnbrasil.com.br/wp-content/uploads/sites/12/2024/03/3.jpeg?w=849&h=477&crop=0"
+                        alt="Projeto Currículo Flexível"
+                        className="w-full h-48 object-cover hover:scale-105 transition-transform duration-300"
+                        />
+                    </div>
+                    <div className="p-6">
+                        <h1 className="font-bold text-lg mb-3 text-gray-800">
+                        Brasil Inicia Projeto-Piloto de “Currículo Flexível Invertido”
+                        </h1>
+                        <p className="text-gray-600 text-sm md:text-base">
+                        O MEC iniciou um projeto-piloto em 50 escolas com um novo modelo onde alunos estudam teoria online e aplicam a prática em sala.
+                        </p>
+                    </div>
+                    </div>
 
                 <div className="bg-white border rounded-lg shadow-lg p-6 hover:shadow-2xl transition-shadow duration-300">
                    <h1 className="font-bold text-lg mb-3 text-gray-800">FDA Aprova Lente de Contato Inteligente que Mede Glicose e Libera Insulina Automaticamente</h1>
