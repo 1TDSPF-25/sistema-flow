40cbf7e2a8006426fe4dcadcc834fe2a61f2d3f8
joaocaetano1310 <joaovictorcaetano1310@gmail.com>
2025-10-14 15:16:23 -0300

    feat: Centralizando cards

--- sistema-flow/src/components/CardNoticia/CardNoticia.tsx
+++ sistema-flow/src/components/CardNoticia/CardNoticia.tsx
@@ -1,18 +1,18 @@
 export default function CardNoticia(){
     return(
-        <div className="flex justify-center w-screen bg-gray-100 py-10">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-5">
+        <div className="flex justify-center w-screen bg-gray-100 py-2">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 px-10 w-600 max-w-7xl"> 
 
             {/* Card 1 */}
-            <div className="bg-white border rounded-lg shadow-lg overflow-hidden hover:shadow-2xl transition-shadow duration-300">
+            <div className="bg-white border rounded-lg shadow-lg overflow-hidden hover:shadow-2xl transition-shadow duration-300 ">
                 <div className="overflow-hidden">
                     <img
                     src="https://lncimg.lance.com.br/cdn-cgi/image/width=950,quality=75,fit=pad,format=webp/uploads/2023/08/2-imagens-44.jpg"
                     alt="Corinthians Campeão Mundial"
-                    className="w-full h-48 object-cover hover:scale-105 transition-transform duration-300"
+                    className="w-full h-35 object-cover hover:scale-105 transition-transform duration-300"
                     />
                 </div>
-                <div className="p-6">
+                <div className="p-3">
                     <h1 className="font-bold text-lg mb-3 text-gray-800">
                     Corinthians Desafia Gigante Europeu e Conquista o Tricampeonato Mundial
                     </h1>
@@ -28,10 +28,10 @@ export default function CardNoticia(){
                         <img
                         src="https://admin.cnnbrasil.com.br/wp-content/uploads/sites/12/2023/10/REBECA-e1698183867468.png?w=419&h=283&crop=0"
                         alt="Rebeca Andrade Ouro Mundial"
-                        className="w-full h-48 object-cover hover:scale-105 transition-transform duration-300"
+                        className="w-full h-35 object-cover hover:scale-105 transition-transform duration-300"
                         />
                     </div>
-                    <div className="p-6">
+                    <div className="p-3">
                         <h1 className="font-bold text-lg mb-3 text-gray-800">
                         Rebeca Andrade Conquista Ouro Inédito no Mundial
                         </h1>
@@ -42,15 +42,15 @@ export default function CardNoticia(){
                     </div>
 
             {/* Card 3 */}
-                <div className="bg-white border rounded-lg shadow-lg overflow-hidden hover:shadow-2xl transition-shadow duration-300">
+                <div className="bg-white border rounded-lg shadow-lg overflow-hidden hover:shadow-2xl transition-shadow duration-300 ">
                     <div className="overflow-hidden">
                         <img
                         src="https://admin.cnnbrasil.com.br/wp-content/uploads/sites/12/2024/03/3.jpeg?w=849&h=477&crop=0"
                         alt="Projeto Currículo Flexível"
-                        className="w-full h-48 object-cover hover:scale-105 transition-transform duration-300"
+                        className="w-full h-35 object-cover hover:scale-105 transition-transform duration-300"
                         />
                     </div>
-                    <div className="p-6">
+                    <div className="p-3">
                         <h1 className="font-bold text-lg mb-3 text-gray-800">
                         Brasil Inicia Projeto-Piloto de “Currículo Flexível Invertido”
                         </h1>
@@ -60,9 +60,23 @@ export default function CardNoticia(){
                     </div>
                     </div>
 
-                <div className="bg-white border rounded-lg shadow-lg p-6 hover:shadow-2xl transition-shadow duration-300">
-                   <h1 className="font-bold text-lg mb-3 text-gray-800">FDA Aprova Lente de Contato Inteligente que Mede Glicose e Libera Insulina Automaticamente</h1>
-                   <p className="text-gray-600 text-sm md:text-base">A agência de saúde dos EUA (FDA) aprovou a "GlucoLens", uma lente de contato inteligente que mede continuamente a glicose na lágrima e libera insulina de forma automática, prometendo revolucionar o tratamento para diabéticos e eliminar a necessidade de agulhas.</p>
+                {/* CARD 4 */}
+                <div className="bg-white border rounded-lg shadow-lg overflow-hidden hover:shadow-2xl transition-shadow duration-300">
+                <div className="overflow-hidden">
+                    <img
+                    src="https://tm.ibxk.com.br/2023/02/03/03114948110520.jpg?img=1920x780"
+                    alt="Lente de Contato Inteligente"
+                    className="w-full h-35 object-cover hover:scale-105 transition-transform duration-300"
+                    />
+                </div>
+                <div className="p-3">
+                    <h1 className="font-bold text-lg mb-3 text-gray-800">
+                    FDA Aprova Lente de Contato Inteligente que Mede Glicose
+                    </h1>
+                    <p className="text-gray-600 text-sm md:text-base">
+                    A FDA aprovou a “GlucoLens”, lente que mede glicose na lágrima e libera insulina automaticamente — um marco para diabéticos.
+                    </p>
+                </div>
                 </div>
 
                 <div className="bg-white border rounded-lg shadow-lg p-6 hover:shadow-2xl transition-shadow duration-300">
