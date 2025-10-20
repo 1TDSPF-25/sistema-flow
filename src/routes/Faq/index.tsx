fffcacf1c217c26f8a8a16b9e1b54cdbaa60169a
ryanvetoriano <vetorianosilva@gmail.com>
2025-10-20 09:51:43 -0300

    feat: Começando a adicionar informações no faq

--- src/routes/Faq/index.tsx
+++ src/routes/Faq/index.tsx
@@ -2,7 +2,14 @@ export default function Faq (){
 
     return(
         <main>
-        
+            <h1>Perguntas frequentes</h1>
+            <section>
+                <div>
+                    <h2>Medicamentos</h2>
+                    <h3>1. Preciso de receita para comprar medicamentos controlados?</h3>
+                    <p>Sim. É obrigatório apresentear receita médica válida para a compra de medicamentos controlados.</p>
+                </div>
+            </section>
         </main>
     )
 }
