28f6bd0de2526d068701b4acbfa5bec2f143426f
cleosales <cleosales10@gmail.com>
2025-10-20 09:46:54 -0300

    estilizando a pagina contatos

--- src/routes/Contatos/index.tsx
+++ src/routes/Contatos/index.tsx
@@ -1,8 +1,8 @@
 function Contatos (){
     return(
-        <main>
-            <h1>Contato</h1>
-            <p>Entre em contato conosco pelo gmail: </p>
+        <main className="min-h-sreen flex flex-col items-center justify-center bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800 text-center p-6">
+            <h1 className="text-4xl font-extrabold test-slate-900 dark:text-slate-100 mb-4">(11) 91254-8855</h1>
+            <p className="text-lg text-slate-600 dark:text-slate-300 max-w-md">Entre em contato conosco pelo gmail: farmacia@gmail.com</p>
         </main>
     )
 }
