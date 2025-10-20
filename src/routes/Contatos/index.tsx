7e359f03968f17f263179286b91eea08e6b93ebd
Martiicolly <nicollymartins118@gmail.com>
2025-10-20 08:54:41 -0300

    Criando a pagina de contatos e montando a estrutura basica da pagina

---
+++ src/routes/Contatos/index.tsx
@@ -0,0 +1,9 @@
+function Contatos (){
+    return(
+        <main>
+            <h1>Contato</h1>
+            <p>Entre em contato conosco pelo gmail: </p>
+        </main>
+    )
+}
+export default Contatos
