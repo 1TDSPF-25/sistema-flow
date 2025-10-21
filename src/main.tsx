e2fb5bcef57f8ae049540d94b940dfb69cd92dbc
pedrohenrique116 <pedrohenriqueluiz9@gmail.com>
2025-10-21 08:18:47 -0300

    feat: Adicionando o Suspense Api

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,43 @@
-import { StrictMode } from 'react';
+import { StrictMode, lazy } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import "./globals.css";
-
+ 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './routes/Home/index.tsx';
-import Produtos from './routes/Produtos/index.tsx';
-import EditarProdutos from './routes/EditarProdutos/index.tsx';
+ 
 import Error from './routes/Error/index.tsx';
-import CadastroFarmacia from './routes/Cadastro/index.tsx';
 import Login from './routes/Login/index.tsx';
-import TipoCompra from './routes/FinalizarCompra/index.tsx';
-import Faq from './routes/Faq/index.tsx';
-
+ 
+const Home = lazy(() => import('./routes/Home/index.tsx'));
+const Produtos = lazy(() => import('./routes/Produtos/index.tsx'));
+const EditarProdutos = lazy(() => import('./routes/EditarProdutos/index.tsx'));
+const CadastroFarmacia = lazy(() => import('./routes/Cadastro/index.tsx'));
+const TipoCompra = lazy(() => import('./routes/FinalizarCompra/index.tsx'));
+const Faq = lazy(() => import('./routes/Faq/index.tsx'));
+ 
+ 
 export function rotaLimitada(elemento: React.ReactElement) {
   return localStorage.getItem('token') ? elemento : <Login />
 }
-
+ 
 const router = createBrowserRouter([
   {
     path: "/", element: <App />, errorElement: <Error />, children: [
       { path: "/", element: <Home /> },
-      { path: "/login", element: <LoginFarmacia /> },
+     
+      { path: "/login", element: <Login /> },
       { path: "/cadastro", element: <CadastroFarmacia /> },
       { path: "/produtos", element: <Produtos /> },
       { path: "/produto/:id", element: <Produtos /> },
-      { path: "/editar/produtos/:id", element: <EditarProdutos /> },
-      { path: "/finalizar-compra", element: <TipoCompra /> }
+   
+      { path: "/editar/produtos/:id", element: rotaLimitada(<EditarProdutos />) },
+      { path: "/finalizar-compra", element: rotaLimitada(<TipoCompra />) },
+     
+      { path: "/faq", element: <Faq /> }
     ]
   }
 ]);
-
+ 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
